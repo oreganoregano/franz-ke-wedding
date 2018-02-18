5531341b11e9932e0bc91c1d562cd18f3e722758
Regan Smith <smith.regan.d@gmail.com>
2018-02-18 18:07:38 -0500

    initial build

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -15,3 +15,20 @@ function showDivs(n) {
     }
     x[slideIndex-1].style.display = "block";
 }
+
+window.onscroll = function() {myFunction()};
+
+// Get the menu
+var menu = document.getElementById("menu");
+
+// Get the offset position of the menu
+var sticky = menu.offsetTop;
+
+// Add the sticky class to the menu when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    menu.classList.add("sticky")
+  } else {
+    menu.classList.remove("sticky");
+  }
+}
